2d462c415fce4c31bd1c4f7dd837da97bedc0f71
David Wojnar <david.wojnar@uni-tuebingen.de>
2015-05-04 10:24:16 +0000

    State implements Serializable to satisfy liferay 6.2 session policy.
    git-svn-id: svn+ssh://sshgw-bs.informatik.uni-tuebingen.de/abi-projects/QBiC/PortalDevelopment/svn/trunk@461 a0440ff8-2b04-4d88-a457-8e570ff6c9eb

--- QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/State.java
+++ QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/State.java
@@ -1,23 +1,31 @@
 package de.uni_tuebingen.qbic.qbicmainportlet;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Observable;
 
 import com.vaadin.ui.UI;
 
-public class State extends Observable {
-	
-	
-	
-	public State() {
-		
-	}
-	
+public class State extends Observable implements Serializable {
 
-	public void notifyObservers(ArrayList<String> message) {
-		// TODO Auto-generated method stub
-		this.setChanged();
-		super.notifyObservers(message);
-		UI.getCurrent().getNavigator().navigateTo(String.format("%s/%s", message.get(2).toLowerCase(), message.get(1)));
-	}
+
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -8448995433087062650L;
+
+
+  public State() {
+
+  }
+
+
+  public void notifyObservers(ArrayList<String> message) {
+    // TODO Auto-generated method stub
+    this.setChanged();
+    super.notifyObservers(message);
+    UI.getCurrent().getNavigator()
+        .navigateTo(String.format("%s/%s", message.get(2).toLowerCase(), message.get(1)));
+  }
 }
