2d4a8b94b699edcb7af85727ecd73b92c1fc0907
David Wojnar <david.wojnar@uni-tuebingen.de>
2015-07-20 11:19:43 +0000

    updated version and added basic description
    git-svn-id: svn+ssh://sshgw-bs.informatik.uni-tuebingen.de/abi-projects/QBiC/PortalDevelopment/svn/trunk@566 a0440ff8-2b04-4d88-a457-8e570ff6c9eb

--- QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/QbicmainportletUI.java
+++ QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/QbicmainportletUI.java
@@ -64,8 +64,8 @@ public class QbicmainportletUI extends UI {
   private VerticalLayout mainLayout;
   private ConfigurationManager manager;
   private logging.Logger LOGGER = new Log4j2Logger(QbicmainportletUI.class);
-  private String version = "0.5";
-  private String revision = "559";
+  private String version = "0.5.1";
+  private String revision = "565";
   private String resUrl;
   protected View currentView;
 
