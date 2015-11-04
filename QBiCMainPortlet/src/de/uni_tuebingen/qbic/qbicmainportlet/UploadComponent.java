727f0313f34a80b5a8e3ec9271f71d7a0a3e193e
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2015-11-04 17:58:34 +0100

    - Attachment View will now check for existing samples first and create the attachment sample if none were found, fixing a bug -- To do this openbis_reg.jar for sample and experiment registration has been added

--- QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/UploadComponent.java
+++ QBiCMainPortlet/src/de/uni_tuebingen/qbic/qbicmainportlet/UploadComponent.java
@@ -2,45 +2,13 @@ package de.uni_tuebingen.qbic.qbicmainportlet;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import logging.Log4j2Logger;
-import logging.Logger;
+import main.OpenBisClient;
 import main.UploadsPanel;
 import model.AttachmentConfig;
-import model.ExperimentBean;
-import model.ProjectBean;
-import model.SampleBean;
-import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.ControlledVocabularyPropertyType;
-import ch.systemsx.cisd.openbis.generic.shared.basic.dto.DataTypeCode;
-import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.PropertyType;
 
-import com.vaadin.data.fieldgroup.FieldGroup;
-import com.vaadin.data.util.IndexedContainer;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.Page;
-import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.shared.ui.MarginInfo;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Field;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.ProgressBar;
-import com.vaadin.ui.Notification.Type;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 import de.uni_tuebingen.qbic.main.ConfigurationManager;
@@ -48,14 +16,8 @@ import de.uni_tuebingen.qbic.main.LiferayAndVaadinUtils;
 
 public class UploadComponent extends CustomComponent {
 
-
-  private DataHandler datahandler;
-  private String resourceUrl;
-  private State state;
   private VerticalLayout mainView;
 
-  private FormLayout form;
-  private FieldGroup fieldGroup;
   VerticalLayout vert;
   String id;
 
@@ -74,16 +36,16 @@ public class UploadComponent extends CustomComponent {
     this.setWidth(Page.getCurrent().getBrowserWindowWidth() * 0.8f, Unit.PIXELS);
   }
 
-  public void updateUI(ConfigurationManager manager, String projectCode) {
+  public void updateUI(ConfigurationManager manager, String projectCode, String space, OpenBisClient openBisClient) {
     AttachmentConfig attachConfig =
         new AttachmentConfig(Integer.parseInt(manager.getAttachmentMaxSize()),
             manager.getAttachmentURI(), manager.getAttachmentUser(), manager.getAttachmenPassword());
 
     mainView =
-        new UploadsPanel(manager.getTmpFolder(), projectCode, new ArrayList<String>(Arrays.asList(
+        new UploadsPanel(manager.getTmpFolder(), space, projectCode, new ArrayList<String>(Arrays.asList(
             "Project Planning", "Results")), LiferayAndVaadinUtils.getUser().getScreenName(),
-            attachConfig);
-
+            attachConfig, openBisClient);
+    
     mainView.setWidth(100.0f, Unit.PERCENTAGE);
     mainView.setMargin(new MarginInfo(true, false, true, true));
     mainView.setSpacing(true);
